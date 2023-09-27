58bd960e13b2c1a7496e0d9e1fa941c0801a2e3d
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-09-28 00:43:59 +0100

    created delete record function

--- infobridge/webapp/views.py
+++ infobridge/webapp/views.py
@@ -56,3 +56,13 @@ def customer_record(request, pk):
 	else:
 		messages.success(request, "You Must be Logged In To View That Page")
 		return redirect('home')
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must be Loggged In")
+		return redirect('home')
